66b5621f83067f08afbb3d9a95b77f5639a4e806
Sergey Dobrynin <SergeyDobrinyn1983@gmail.com>
2021-03-30 05:13:26 +0300

    Functions are working.

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,18 @@ let path = {
     fonts: project_folder + "/fonts/",
   },
   src: {
-    html: source_folder + "/*.html",
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
     css: source_folder + "/scss/main.scss",
     js: source_folder + "/js/script.js",
     img: source_folder + "/img/**/*.{jpg,jpeg,png,gif,svg,ico,webp}",
-    fonts: source_folder + "/fonts/",
+    fonts: source_folder + "/fonts/**/*",
   },
   watch: {
     html: source_folder + "/**/*.html",
     css: source_folder + "/scss/**/*.scss",
     js: source_folder + "js/**/*.js",
-    img: source_folder + "img/**/*.{jpg,jpeg,png,gif,svg,ico,webp}"
+    img: source_folder + "img/**/*.{jpg,jpeg,png,gif,svg,ico,webp}",
+    fonts: source_folder + "/fonts/"
   },
   clean: "./" + project_folder + "/"
 }
@@ -30,7 +31,13 @@ let {src, dest} = require("gulp"),
   browsersync = require("browser-sync").create(),
   del = require("del"),
   scss = require("gulp-sass"),
-  autoprefixer = require("gulp-autoprefixer");
+  autoprefixer = require("gulp-autoprefixer"),
+  fileinclude = require("gulp-file-include"),
+  media = require("gulp-group-css-media-queries"),
+  cssmin = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  jsmin = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin");
 
 // * Syncing with browsers
 function sync() {
@@ -47,6 +54,7 @@ function sync() {
 // * Creating dist directory
 function html() {
   return src(path.src.html)
+    .pipe(fileinclude())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 }
@@ -55,9 +63,9 @@ function html() {
 function files() {
   gulp.watch([path.watch.html],html);
   gulp.watch([path.watch.css],css);
-  // * gulp.watch([path.watch.js],js);
-  // * gulp.watch([path.watch.img],images);
-  // * gulp.watch([path.watch.fonts],fonts);
+  gulp.watch([path.watch.js],js);
+  gulp.watch([path.watch.img],images);
+  gulp.watch([path.watch.fonts],fonts);
 }
 
 // ! Deleting dist directory
@@ -75,13 +83,54 @@ function css() {
       overrideBrowserslist: ["last 5 versions"],
       cascade: true
     }))
+    .pipe(media())
+    .pipe(dest(path.build.css))
+    .pipe(cssmin())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
     .pipe(dest(path.build.css))
     .pipe(browsersync.stream())
 }
 
-let build = gulp.series(clean,gulp.parallel(css,html));
+// * JavaScript
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(jsmin())
+    .pipe(rename({
+      extname: ".min.js"
+    }))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
+// * Images
+function images() {
+  return src(path.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      optimizationLevel: 3
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+// * Fonts
+function fonts() {
+  return src(path.src.fonts)
+    .pipe(dest(path.build.fonts))
+    .pipe(browsersync.stream())
+}
+
+let build = gulp.series(clean,gulp.parallel(fonts,images,js,css,html));
 let watch = gulp.parallel(build,files,sync);
 
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
